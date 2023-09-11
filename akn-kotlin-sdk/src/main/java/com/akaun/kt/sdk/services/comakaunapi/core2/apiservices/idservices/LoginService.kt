e40471516c796ec71c7a2817370c156c33bbc34a
Shyam Borkar <shyamborkar@wavelet.net>
2023-09-11 10:52:11 +0800

    Changed the return of the login with call to be wrapped around BasicApiResponseModel

--- akn-kotlin-sdk/src/main/java/com/akaun/kt/sdk/services/comakaunapi/core2/apiservices/idservices/LoginService.kt
+++ akn-kotlin-sdk/src/main/java/com/akaun/kt/sdk/services/comakaunapi/core2/apiservices/idservices/LoginService.kt
@@ -1,6 +1,7 @@
 package com.akaun.kt.sdk.services.comakaunapi.core2.apiservices.idservices
 
 import com.akaun.kt.sdk.models.dbschema.LoginRequest
+import com.akaun.kt.sdk.services.comakaunapi.core2.apiservices.shared.BasicApiResponseModel
 import com.akaun.kt.sdk.services.comakaunapi.core2.apiservices.shared.Core2Config
 import com.akaun.kt.sdk.services.comakaunapi.core2.apiservices.shared.LoginResponse
 import retrofit2.Call
@@ -12,9 +13,9 @@ import retrofit2.http.Query
 interface LoginService {
     @POST(Core2Config.PLATFORM_DOMAIN_URL_PREFIX + "identity/login")
     fun loginWithCall(@Body loginRequest: LoginRequest
-              , @Query("tenantCode") tenantCode :String? = null) : Call<LoginResponse>
+              , @Query("tenantCode") tenantCode :String? = null) : Call<BasicApiResponseModel<LoginResponse>>
 
     @POST(Core2Config.PLATFORM_DOMAIN_URL_PREFIX + "identity/login")
     fun login(@Body loginRequest: LoginRequest
-              , @Query("tenantCode") tenantCode :String? = null) : LoginResponse
+              , @Query("tenantCode") tenantCode :String? = null) : BasicApiResponseModel<LoginResponse>
 }

c7303ddc002c2c6e73a89aef1e5a60d4508836c0
Shyam Borkar <shyamborkar@wavelet.net>
2023-09-18 09:36:58 +0800

    Added missing generic type in DataOrException wrapper.

--- akn-kotlin-sdk/src/main/java/com/akaun/kt/sdk/utils/wrapper/DataOrException.kt
+++ akn-kotlin-sdk/src/main/java/com/akaun/kt/sdk/utils/wrapper/DataOrException.kt
@@ -1,6 +1,6 @@
 package com.akaun.kt.sdk.utils.wrapper
 
-data class DataOrException<T, E : Exception>(
+data class DataOrException<T, Boolean, E : Exception>(
     val data: T? = null,
     val loading: Boolean? = null,
     val e: E? = null

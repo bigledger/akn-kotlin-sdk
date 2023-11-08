39d609cc936506ae067f4a04cc67a8ee87239b91
Tan Jun Yu <junyu@wavelet.net>
2023-11-08 15:25:39 +0800

    Changed all attributes to var from val

--- akn-kotlin-sdk/src/main/java/com/akaun/kt/sdk/models/dbschema/bl_del_shipment_processing_open_queue.kt
+++ akn-kotlin-sdk/src/main/java/com/akaun/kt/sdk/models/dbschema/bl_del_shipment_processing_open_queue.kt
@@ -1,20 +1,20 @@
 package com.akaun.kt.sdk.models.dbschema
 
 data class bl_del_shipment_processing_open_queue(
-    val acl_config: Any? = null,
-    val acl_policy: Any? = null,
-    val applet_guid: Any? = null,
-    val bal_qty: Double? = null,
-    val created_by_subject_guid: String? = null,
-    val created_date: String? = null,
-    val guid: String? = null,
-    val module_guid: Any? = null,
-    val namespace: Any? = null,
-    val property_json: Any? = null,
-    val revision: String? = null,
-    val shipment_hdr_guid: String? = null,
-    val status: String? = null,
-    val updated_by_subject_guid: String? = null,
-    val updated_date: String? = null,
-    val vrsn: Any? = null
+    var acl_config: Any? = null,
+    var acl_policy: Any? = null,
+    var applet_guid: Any? = null,
+    var bal_qty: Double? = null,
+    var created_by_subject_guid: String? = null,
+    var created_date: String? = null,
+    var guid: String? = null,
+    var module_guid: Any? = null,
+    var namespace: Any? = null,
+    var property_json: Any? = null,
+    var revision: String? = null,
+    var shipment_hdr_guid: String? = null,
+    var status: String? = null,
+    var updated_by_subject_guid: String? = null,
+    var updated_date: String? = null,
+    var vrsn: Any? = null
 )
--- akn-kotlin-sdk/src/main/java/com/akaun/kt/sdk/utils/wrapper/DataOrException.kt
+++ akn-kotlin-sdk/src/main/java/com/akaun/kt/sdk/utils/wrapper/DataOrException.kt
@@ -1,7 +1,7 @@
 package com.akaun.kt.sdk.utils.wrapper
 
 data class DataOrException<T, Boolean, E : Exception>(
-    val data: T? = null,
-    val loading: Boolean? = null,
-    val e: E? = null
+    var data: T? = null,
+    var loading: Boolean? = null,
+    var e: E? = null
 )

5eba1209039d5ed238c2a835c5028a072d565360
Shyam Kamalesh Borkar <shyamborkar@wavelet.net>
2023-11-08 17:13:41 +0800

    Added custom interceptor functionality in RetrofitClient.

--- akn-kotlin-sdk/src/main/java/com/akaun/kt/sdk/utils/client/RetrofitClients.kt
+++ akn-kotlin-sdk/src/main/java/com/akaun/kt/sdk/utils/client/RetrofitClients.kt
@@ -1,28 +1,35 @@
 package com.akaun.kt.sdk.utils.client
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     const val requestTimeout: Long = 30
 
-    fun getAuthorizedClient(authorization: String, tenantCode: String): OkHttpClient {
+    fun getAuthorizedClient(
+        authorization: String,
+        tenantCode: String,
+        loggingInterceptor: HttpLoggingInterceptor = LoggingInterceptor.bodyLoggingInterceptor
+    ): OkHttpClient {
         val authorziedLoggingClient = OkHttpClient.Builder()
             .connectTimeout(requestTimeout, TimeUnit.SECONDS) // Set the connection timeout
             .readTimeout(requestTimeout, TimeUnit.SECONDS)    // Set the read timeout
             .writeTimeout(requestTimeout, TimeUnit.SECONDS)   // Set the write timeout
-            .addInterceptor(LoggingInterceptor.bodyLoggingInterceptor)
+            .addInterceptor(loggingInterceptor)
             .addInterceptor(HeaderInterceptor(authorization, tenantCode))
             .build()
         return authorziedLoggingClient
     }
 
-    fun getBasicClient(): OkHttpClient {
+    fun getBasicClient(
+        loggingInterceptor: HttpLoggingInterceptor = LoggingInterceptor.bodyLoggingInterceptor
+    ): OkHttpClient {
         val basicClient = OkHttpClient.Builder()
             .connectTimeout(requestTimeout, TimeUnit.SECONDS) // Set the connection timeout
             .readTimeout(requestTimeout, TimeUnit.SECONDS)    // Set the read timeout
             .writeTimeout(requestTimeout, TimeUnit.SECONDS)   // Set the write timeout
-            .addInterceptor(LoggingInterceptor.bodyLoggingInterceptor)
+            .addInterceptor(loggingInterceptor)
             .build()
         return basicClient
     }
